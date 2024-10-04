54ee05670ee56026bc22aaecd4de6547e4a040c5
mrDezz <deepakguptaswork@gmail.com>
2024-10-05 01:21:58 +0530

    ADD:zodiac sign compponent

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,15 +1,24 @@
 import HomeBanner from "@/components/banners/HomeBanner";
+import SignsCard from "@/components/cards/SignsCard";
 import HomeHeader from "@/components/headers/homeHeader";
 import HorizontalGrid from "@/components/hocs/HorizontalGrid";
 import { primaryDarkColor } from "@/constants/Colors";
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Link } from "expo-router";
 
 const HomeScreen = () => {
   return (
     <View style={HomeStyle.ContainerStyle}>
-      <HomeHeader />
-      <HomeBanner />
+      <SignsCard
+        label="aries"
+        children={
+          <MaterialCommunityIcons name="zodiac-aries" size={24} color="black" />
+        }
+      />
+      {/* <HomeHeader />
+      <HomeBanner /> */}
       {/* horizontal with see all button // Todo : Top astrologer cards with scroll */}
       {/* <HorizontalGrid
       title="Horoscope"
